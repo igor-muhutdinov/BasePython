11917944f83d8aaa4ddaa40a6fdc5626bfb75d8e
Igor Muhutdinov <igor.muhutdinov@yandex.ru>
2021-04-17 16:51:02 +0900

    homework_01: Function power_numbers realized

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,14 +4,15 @@
 """
 
 
-def power_numbers():
+def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-
+    return [i*2 for i in numbers]
+    
 
 # filter types
 ODD = "odd"
