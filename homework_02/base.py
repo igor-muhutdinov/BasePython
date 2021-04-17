7d2a88a34fafe02d2898bbf79b31ac4b5a396b74
Igor Muhutdinov <igor.muhutdinov@yandex.ru>
2021-04-17 22:06:24 +0900

    homework_02: Added attrs and methods to class base.Vehicle

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,28 @@
 from abc import ABC
+from homework_02 import exceptions
 
 
 class Vehicle(ABC):
-    pass
+    weight = 0
+    started = False
+    fuel = 0
+    fuel_consumption = 0
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if self.started is False:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise exceptions.LowFuelError
+
+    def move(self, distance):
+        necessary_fuel = distance / 100 * self.fuel_consumption
+        if necessary_fuel <= self.fuel:
+            self.fuel -= necessary_fuel
+        else:
+            raise exceptions.NotEnoughFuel
