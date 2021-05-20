39334e0e01df332e443bcd828de35e8f39b0d40e
Igor Muhutdinov <igor.muhutdinov@yandex.ru>
2021-05-20 21:58:40 +0900

    homework_03: Created classes User, Post

--- homework_03/models.py
+++ homework_03/models.py
@@ -17,9 +17,33 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 engine = create_async_engine(PG_CONN_URI, echo=True)
 Base = declarative_base(engine)
 Session = sessionmaker(engine)
 
+
+class User(Base):
+    __tablename__ = "users"
+    __mapper_args__ = {"eager_defaults": True}
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    username = Column(String)
+    email = Column(String)
+
+    posts = relationship("Post")
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    __mapper_args__ = {"eager_defaults": True}
+
+    user_id = Column(ForeignKey("users.id"))
+    title = Column(String)
+    body = Column(String)
+
+    users = relationship("User")
