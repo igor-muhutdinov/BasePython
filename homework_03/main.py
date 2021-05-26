387450182081687dc0724a898a5618dbd87979d1
Igor Muhutdinov <igor.muhutdinov@yandex.ru>
2021-05-26 20:44:29 +0900

    homework_03: Implemented functions for adding users and posts data to db

--- homework_03/main.py
+++ homework_03/main.py
@@ -14,13 +14,50 @@
 """
 
 import asyncio
-import models
+from models import create_tables, engine, User, Post
 from jsonplaceholder_requests import get_users_data_from_url, get_posts_data_from_url
+from json import JSONDecoder
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def async_main():
-    await models.create_tables()
+    await create_tables()
     users, posts = await asyncio.gather(get_users_data_from_url(), get_posts_data_from_url())
+    await add_users_to_db_from_json(users)
+    await add_posts_to_db_from_json(posts)
+
+
+async def add_users_to_db_from_json(users: str):
+    json_decoder = JSONDecoder()
+    users_list = json_decoder.decode(users)
+
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with async_session() as session:
+        session: AsyncSession
+        async with session.begin():
+            for single_user in users_list:
+                user = User(name=single_user['name'],
+                            username=single_user['username'],
+                            email=single_user['email'])
+                session.add(user)
+
+
+async def add_posts_to_db_from_json(posts: str):
+    json_decoder = JSONDecoder()
+    posts_list = json_decoder.decode(posts)
+
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with async_session() as session:
+        session: AsyncSession
+        async with session.begin():
+            for single_post in posts_list:
+                post = Post(user_id=single_post['userId'],
+                            title=single_post['title'],
+                            body=single_post['body'])
+                session.add(post)
 
 
 def main():
